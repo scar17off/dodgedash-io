8d239a68b166fd4ac313f493e3850b68e165b49f
scar17off <scar17off@gmail.com>
2024-07-25 01:56:32 +0300

    Establishing the project environment

---
+++ README.md
@@ -0,0 +1,2 @@
+# DodgeDash.io
+Welcome to DodgeDash.io, an exciting game where you navigate through various areas, evade enemies, and save players to help them progress!
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+});
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+httpServer.listen(443, () => {
+	console.log("Server is running on port 443");
+});
